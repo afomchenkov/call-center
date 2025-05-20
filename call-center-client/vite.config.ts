e89cf5a243182e096583ba1c13c1229f0c7cbcb5
Aliaksandr Famchankou <aliaksandr.famchankou@gmail.com>
2025-05-20 16:53:59 +0200

    feat: update vite config and dependencies

--- call-center-client/vite.config.ts
+++ call-center-client/vite.config.ts
@@ -8,7 +8,16 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@models': path.resolve(__dirname, 'src/models'),
+      '@types': path.resolve(__dirname, 'src/types'),
+      '@state': path.resolve(__dirname, 'src/state'),
+      '@schemas': path.resolve(__dirname, 'src/schemas'),
+      '@api': path.resolve(__dirname, 'src/api'),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 })
