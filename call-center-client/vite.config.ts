1552c255991433f18bd626d37f8bc4dbf1a89c5c
Aliaksandr Famchankou <aliaksandr.famchankou@gmail.com>
2025-06-02 08:39:08 +0200

    chore: update test config

--- call-center-client/vite.config.ts
+++ call-center-client/vite.config.ts
@@ -1,11 +1,18 @@
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    include: ['**/*.test.ts', '**/*.test.tsx'],
+    exclude: ['node_modules'],
+  },
   resolve: {
     alias: {
       '@hooks': path.resolve(__dirname, 'src/hooks'),
